78f6423524b1c1560d05d1a85b6305269206dda2
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-01-29 17:10:48 +0000

    changes 4

--- tests/zara_store_test.rb
+++ tests/zara_store_test.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require_relative '/workspaces/Zara-Store [Codespaces: musical happiness]/zara_store.rb/' 
+require_relative '/workspaces/Zara-Store [Codespaces: musical happiness]/zara_store.rb/'
 
 class TestZaraStore < Minitest::Test
   def setup
