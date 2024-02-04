cc864112e665037f2d6bc9c59a6c873fb71a0ba4
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-02-04 04:47:20 +0000

    changes 11

--- zara_store.rb
+++ zara_store.rb
@@ -9,70 +9,26 @@ class ZaraStore
     }
   end
 
-  def welcome_message
-    puts "\n"
-    puts "Welcome to Zara!"
-    puts "How may we assist you today?"
-    customer_interaction
-  end
+  def check_availability(item)
+    return unless @inventory.key?(item)
 
-  def customer_interaction
-    loop do
-      puts "\n"
-      puts "1. Check item availability"
-      puts "2. Inquire about delivery"
-      puts "3. Return and Exchange Policy"
-      puts "4. Exit"
-      puts "Choose an option: "
-      choice = gets.chomp.to_i
-
-      case choice
-      when 1
-        check_availability_interaction
-      when 2
-        inquire_about_delivery
-      when 3
-        return_exchange_policy
-      when 4
-        puts "Thank you for visiting Zara. Have a great day!"
-        break
-      else
-        puts "Invalid option. Please choose a valid option."
-      end
-    end
-  end
-
-  def check_availability_interaction
-    puts "\n"
-    puts "What item are you interested in?"
-    item = gets.chomp.downcase
-    number_of_items(item)
-  end
-
-  def number_of_items(item)
-    if @inventory.key?(item)
-      puts "\n"
-      puts "Great choice! We have #{@inventory[item][:availability]} #{item}s available."
-      puts "Each #{item} costs $#{@inventory[item][:price]}."
-      return @inventory[item][:availability]
-    else
-      puts "\n"
-      puts "Sorry, we don't have #{item} in our inventory at the moment."
-      return 0
-    end
+    {
+      available: true,
+      message: "Great choice! We have #{@inventory[item][:availability]} #{item}s available.",
+      price: @inventory[item][:price],
+      availability: @inventory[item][:availability],
+    }
   end
 
   def inquire_about_delivery
-    puts "\n"
-    puts "Our standard delivery time is 3-5 business days."
-    puts "For expedited delivery options, please contact our customer service."
+    {
+      message: "Our standard delivery time is 3-5 business days. For expedited delivery options, please contact our customer service.",
+    }
   end
 
   def return_exchange_policy
-    puts "\n"
-    puts "Our return and exchange policy is as follows:"
-    puts "For in-store purchases, you can return items within 30 days for a full refund."
-    puts "After 30 days, store credit will be provided for in-store purchases."
-    puts "For online purchases, a 30-day full refund policy applies with no exceptions."
+    {
+      message: "Our return and exchange policy is as follows: For in-store purchases, you can return items within 30 days for a full refund. After 30 days, store credit will be provided for in-store purchases. For online purchases, a 30-day full refund policy applies with no exceptions.",
+    }
   end
 end
